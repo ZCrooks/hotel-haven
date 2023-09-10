9ca2c45a1d93f15f4c8c190b4a44b3e5d903105f
ZCrooks <zachthedevguy@gmail.com>
2023-09-10 15:49:41 -0400

    successfully storing user emails (subscriptions to newsletter) in Firebase Firestore

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -6,7 +6,36 @@ import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
+import { db } from '../Firebase';
+import { addDoc, collection, serverTimestamp} from 'firebase/firestore';
+
 const Newsletter = () => {
+
+    // Store User Input (email) in State
+    const [input, setInput] = useState("")
+
+    // Set User Input in State
+    const handleInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // add to firebase Firestore
+        if (input) {
+            await addDoc(collection(db, "newsletter emails"), {
+                email: input,
+                timestamp: serverTimestamp()
+            })
+            .then(() => {
+                alert("Thank you for subscribing! Hotel & Flight details coming your way!")
+            })
+            setInput("")
+        }
+    }
+
     return (
         <section className="newsletter-section">
             <Container>
@@ -14,7 +43,6 @@ const Newsletter = () => {
                     <Col>
                         <h2>Join our newsletter &#127881;</h2>
                         <p>Read to gain new perspectives on the hottest locations, experiences, and hotel deals!</p>
-                   
                             <div className="link-box">
                                 <Button variant="info">01</Button> 
                                 <p>Get exclusive Hotel Offers!</p>                               
@@ -23,17 +51,17 @@ const Newsletter = () => {
                                 <Button variant="info" className="link-box-2">02</Button> 
                                 <p>Limited time Flight deals</p>                                 
                             </div>
-                             <Form className="newsletter-form">
+                             <Form className="newsletter-form" method="POST" onSubmit={handleSubmit}  >
                                 <Row className="g-2">
                                     <Col md>
-                                        <Form.Control size="lg" type="text" placeholder="Enter your Email" />
+                                        <Form.Control size="lg" type="text" placeholder="Enter your Email" onChange={handleInput} />
                                     </Col>  
-                                    <Button type="submit" className="newsletter-submit-button"><FontAwesomeIcon icon={faMagnifyingGlass} />
+                                    <Button type="submit" className="newsletter-submit-button"><FontAwesomeIcon icon={faMagnifyingGlass}/>
                                     </Button> 
                                 </Row>
                             </Form>
                     </Col>
-                    <Col>
+                    <Col className="newsletter-image">
                         <img src={NewsletterImage} alt="" />
                     </Col>
                 </Row>
